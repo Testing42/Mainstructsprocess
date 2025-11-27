99bfdb6da0d3d7d0ed8f161f72ed93b9a19a8623
Andres Morales <meliand11@yahoo.com>
2025-11-26 23:56:42 -0500

    beginning the addition of todo functionality

---
+++ todo/todo.go
@@ -0,0 +1,52 @@
+package note
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+// for marshall to work all the items in struct must be capital
+/* struct tags are meta data
+that can be added to structs below
+so that fields can be given any
+keta data you want be if you don't
+have code that can manage that meta
+data then nothing will happen.
+*/
+type Todo struct {
+	Text string `json:"Text"`
+}
+
+//this become a method by adding
+//(somethinginside) that passes cvalues from the note struct
+// the word before the name of the struct is a nickname
+// that
+
+func (todo Todo) Display() {
+	fmt.Println(todo.Text)
+}
+
+func (todo Todo) Save() error {
+	fileName := "todo.json"
+
+	json, err := json.Marshal(todo)
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, json, 0644)
+
+}
+
+func NewTodo(content string) (*Todo, error) {
+	if content == "" {
+		return &Todo{}, errors.New("invalid input")
+	}
+
+	return &Todo{
+		Text: content,
+	}, nil
+}
